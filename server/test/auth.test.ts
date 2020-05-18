9562ba0c9d1a3f010b4e49c95f53629d1f349873
vladenego <mistervlad3@gmail.com>
2020-05-19 00:15:01 +0300

    Add test for login

--- server/test/auth.test.ts
+++ server/test/auth.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { createDatabase } from '../src/database'
+const jwt = require('jsonwebtoken')
 
 // drop database and start server before each test run
 beforeAll(async () => {
@@ -33,14 +34,32 @@ describe('auth', () => {
     })
   })
 
-  // // TODO
-  // describe('login', () => {
-  //   it('rejects empty credentials', async () => {
-  //     // TODO
-  //   })
+  // TODO
+  describe('login', () => {
+    it('rejects empty credentials', async () => {
+      await request('http://localhost:8080')
+        .post('/auth/login')
+        .send({
+          email: '',
+          password: '',
+        })
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toEqual('invalid credentials')
+        })
+    })
 
-  //   it('logs in user and returns token', async () => {
-  //     // TODO
-  //   })
-  // })
+    it('logs in user and returns token', async () => {
+      await request('http://localhost:8080')
+        .post('/auth/login')
+        .send({
+          email: 'tester@example.com',
+          password: 'super-secret-password',
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(jwt.decode(res.body.token)).toHaveProperty('_id')
+        })
+    })
+  })
 })
