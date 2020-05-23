a2303ff471fa868e1a941cf4e253d5169134bfca
vladenego <mistervlad3@gmail.com>
2020-05-23 14:00:27 +0300

    feat(posts): Added test to posts: Create, Update and Delete posts

--- server/test/posts.test.ts
+++ server/test/posts.test.ts
@@ -7,48 +7,80 @@ beforeAll(async () => {
   await database.dropDatabase()
 })
 
-describe('CRUD Posts', () => {
-  describe('create post and receive its ID', () => {
-    it('send post to DB', async () => {
-      await request('http://localhost:8080')
-        .post('/posts')
-        .send({
-          title: 'test title',
-          description: 'test description',
-        })
-        .expect(200)
-        .expect((res) => {
-          expect(res.body).toHaveProperty('postID')
-        })
-    })
+let postId = ''
+
+describe('Posts', () => {
+  it('Create post', async () => {
+    await request('http://localhost:8080')
+      .post('/posts')
+      .send({
+        title: 'test title',
+        description: 'test description',
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toHaveProperty('postID')
+        postId = res.body.postID
+        console.log(postId)
+      })
   })
 
-  describe('CRUD posts', () => {
-    it('rejects empty credentials', async () => {
-      await request('http://localhost:8080')
-        .post('/posts/:postId')
-        .send({
-          // _id: req.params.postId,
-          email: '',
-          password: '',
-        })
-        .expect(200)
-        .expect((res) => {
-          expect(res.body).toHaveProperty('_id')
-        })
-    })
+  it('Update post', async () => {
+    const postBeforeUpdate = await request('http://localhost:8080')
+      .get(`/posts/${postId}`)
+      .expect(200)
 
-  //   it('logs in user and returns token', async () => {
-  //     await request('http://localhost:8080')
-  //       .post('/auth/login')
-  //       .send({
-  //         email: 'tester@example.com',
-  //         password: 'super-secret-password',
-  //       })
-  //       .expect(200)
-  //       .expect((res) => {
-  //         expect(jwt.decode(res.body.token)).toHaveProperty('_id')
-  //       })
-  //   })
-  // })
+    await request('http://localhost:8080')
+      .patch(`/posts/${postId}`)
+      .send({
+        title: 'Updated title',
+        description: 'Updated description',
+        date: Date.now(),
+      })
+      .expect(200)
+
+    const postAfterUpdate = await request('http://localhost:8080')
+      .get(`/posts/${postId}`)
+      .expect(200)
+    expect(postBeforeUpdate).not.toMatchObject(postAfterUpdate)
+  })
+
+  it('Delete post', async () => {
+    await request('http://localhost:8080').delete(`/posts/${postId}`).expect(200)
+    await request('http://localhost:8080')
+      .get('/posts/')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.posts.find((element) => element._id == postId)).toBeFalsy()
+      })
+  })
 })
+
+// describe('CRUD posts', () => {
+//   it('rejects empty credentials', async () => {
+//     await request('http://localhost:8080')
+//       .post('/posts/:postId')
+//       .send({
+//         // _id: req.params.postId,
+//         email: '',
+//         password: '',
+//       })
+//       .expect(200)
+//       .expect((res) => {
+//         expect(res.body).toHaveProperty('_id')
+//       })
+//   })
+
+//   it('logs in user and returns token', async () => {
+//     await request('http://localhost:8080')
+//       .post('/auth/login')
+//       .send({
+//         email: 'tester@example.com',
+//         password: 'super-secret-password',
+//       })
+//       .expect(200)
+//       .expect((res) => {
+//         expect(jwt.decode(res.body.token)).toHaveProperty('_id')
+//       })
+//   })
+// })
