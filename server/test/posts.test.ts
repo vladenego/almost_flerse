0eb1d667e4baac2dd25281a4b813b7fcc96b9f77
Ilya Atamas <stackdumper@gmail.com>
2020-05-27 13:42:05 +0300

    test: generate random posts

--- server/test/posts.test.ts
+++ server/test/posts.test.ts
@@ -1,36 +1,22 @@
 import request from 'supertest'
+import casual from 'casual'
 import { createDatabase } from '~/database'
 import { TPost } from '~/types'
 
 // drop database and start server before each test run
 beforeAll(async () => {
   const database = await createDatabase()
-  // await database.dropDatabase()
+  await database.dropDatabase()
 })
 
 let context = {
-  post: [
-    {
-      title: 'test title',
-      description: 'test description',
-    } as TPost,
-    {
-      title: 'test title',
-      description: 'test description',
-    } as TPost,
-    {
-      title: 'test title',
-      description: 'test description',
-    } as TPost,
-    {
-      title: 'test title',
-      description: 'test description',
-    } as TPost,
-    {
-      title: 'test title',
-      description: 'test description',
-    } as TPost,
-  ],
+  posts: Array.from({ length: 10 }).map(
+    () =>
+      ({
+        title: casual.title,
+        description: casual.description,
+      } as TPost),
+  ),
 
   updatedPost: {
     title: 'updated test title',
@@ -41,7 +27,7 @@ let context = {
 describe('posts', () => {
   // create post, update context ids
   it('create post', async () => {
-    for (const item of context.post) {
+    for (const item of context.posts) {
       await request('http://localhost:8080')
         .post('/posts')
         .send(item)
@@ -49,7 +35,7 @@ describe('posts', () => {
         .expect((res) => {
           expect(res.body).toHaveProperty('postID')
 
-          context.post[0]._id = res.body.postID
+          context.posts[0]._id = res.body.postID
           context.updatedPost._id = res.body.postID
         })
     }
@@ -59,7 +45,7 @@ describe('posts', () => {
 // update post
 it('update post', async () => {
   await request('http://localhost:8080')
-    .patch(`/posts/${context.post[0]._id}`)
+    .patch(`/posts/${context.posts[0]._id}`)
     .send(context.updatedPost)
     .expect(200)
 })
@@ -67,11 +53,11 @@ it('update post', async () => {
 // get post, check if updated
 it('get post', async () => {
   await request('http://localhost:8080')
-    .get(`/posts/${context.post[0]._id}`)
+    .get(`/posts/${context.posts[0]._id}`)
     .expect(200)
     .expect((res) => {
-      expect(context.post[0]._id).toEqual(res.body.post._id)
-      expect(context.post).not.toMatchObject(res.body.post)
+      expect(context.posts[0]._id).toEqual(res.body.post._id)
+      expect(context.posts).not.toMatchObject(res.body.post)
     })
 })
 
@@ -81,8 +67,6 @@ it('list posts', async () => {
     .get('/posts/')
     .expect(200)
     .expect((res) => {
-      console.log(res.body.posts[0])
-
       expect(res.body.posts[res.body.posts.length - 1]).toMatchObject(context.updatedPost)
     })
 })
@@ -95,25 +79,25 @@ it('pagination', async () => {
     .expect(200)
     .expect((res) => {
       expect(res.body.posts).toHaveLength(limit)
-      expect(res.body.posts.find((post) => post._id == context.post[0]._id)).toBeFalsy()
-      expect(res.body.posts.find((post) => post._id == context.post[1]._id)).toBeFalsy()
+      expect(res.body.posts.find((post) => post._id == context.posts[0]._id)).toBeFalsy()
+      expect(res.body.posts.find((post) => post._id == context.posts[1]._id)).toBeFalsy()
     })
 })
 
 // delete post
 it('delete post', async () => {
   await request('http://localhost:8080')
-    .delete(`/posts/${context.post[0]._id}`)
+    .delete(`/posts/${context.posts[0]._id}`)
     .expect(200)
 
   // should return 404, post was deleted
-  await request('http://localhost:8080').get(`/posts/${context.post[0]._id}`).expect(404)
+  await request('http://localhost:8080').get(`/posts/${context.posts[0]._id}`).expect(404)
 
   // should not contain post
   await request('http://localhost:8080')
     .get('/posts/')
     .expect(200)
     .expect((res) => {
-      expect(res.body.posts).toHaveLength(context.post.length - 1)
+      expect(res.body.posts).toHaveLength(context.posts.length - 1)
     })
 })
